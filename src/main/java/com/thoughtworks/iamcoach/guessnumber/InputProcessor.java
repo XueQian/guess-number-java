3e3cf0a46a4e97f8a881146b0194aff11587b46c
Xue Qian <xueqian0321@126.com>
2014-10-16 21:58:59 +0800

    add should_return_a_string test at InputProcessorTest.java

---
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/InputProcessor.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.iamcoach.guessnumber;
+
+/**
+ * Created by xueqian on 10/16/14.
+ */
+public class InputProcessor {
+    public String process(){
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/guessnumber/InputProcessorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/InputProcessorTest.java
@@ -1,7 +1,20 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
-/**
- * Created by xueqian on 10/16/14.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class InputProcessorTest {
+
+    @Test
+    public void should_return_a_string(){
+
+        InputProcessor inputProcessor = mock(InputProcessor.class);
+
+        when(inputProcessor.process()).thenReturn("1111");
+        assertThat(inputProcessor.process()).isEqualTo("1111");
+    }
+
 }
